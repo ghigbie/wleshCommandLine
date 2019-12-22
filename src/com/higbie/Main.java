1a642d1ee27a5f9954a6bbee110994ab0f81956a
ghigbie <gehigbie@gmail.com>
2019-12-21 18:29:33 -0700

    Add new methods

--- src/com/higbie/Main.java
+++ src/com/higbie/Main.java
@@ -1,16 +1,29 @@
 package com.higbie;
 
 import com.higbie.game_play.StartGame;
+import com.higbie.models.Player;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         boolean isPlaying = true;
 
+
         StartGame.loadGame();
+
+        Scanner scanner = new Scanner(System.in);
+        String playerName = scanner.next();
+
+        Player player = new Player(playerName, 10, 100, 100);
+        player.declarePlayer();
+
+
         while(isPlaying){
 
         }
 
     }
+
 }
--- src/com/higbie/models/Player.java
+++ src/com/higbie/models/Player.java
@@ -1,5 +1,8 @@
 package com.higbie.models;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Player {
 
     public Player(String name, int power, int life, int maxLife) {
@@ -50,9 +53,21 @@ public class Player {
 
     }
 
-    private void declarePlayer(){
-        System.out.println("You are the great" + this.name + "!");
+    public void declarePlayer(){
+        System.out.println("You realize who you are. You are the great" + this.name + "!");
+        System.out.println("You yell out your name in a roar and see the shock on your enemy's face.");
+        System.out.println("In a renewed vigor your power and strength come flushing your your body!");
         System.out.println("Your battlePower is " + this.battlePower + ".");
         System.out.println("Your life is " + this.life + " of a maximum life of " + this.maxLife );
+    }
+
+    public void firstBattle(){
+        boolean willFight;
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You must kill this Saxon pig! You can either pick up your spear or run.");
+        System.out.println("Enter \"1\" to fight or \"2\" to run.");
+
+
     }
 }
